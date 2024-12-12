1fa862eb1ff039c0d0ee71a921808b4f32ab4e75
dlehdg <ldg2409@naver.com>
2024-12-12 21:42:08 +0900

    [faet] 퀴즈 데이터 추가

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Card = () => {
+  return (
+    <>
+      <Front>
+        <span>앞면</span>
+      </Front>
+
+      <Back>
+        <spna>뒷면</spna>
+      </Back>
+    </>
+  )
+}
+
+export default Card
+
+
+const Front = styled.div`
+
+`;
+
+const Back = styled.div`
+
+`;
